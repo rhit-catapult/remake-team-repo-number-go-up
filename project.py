e138a25f3513cd234b4b161f85cc9d7030bcf9f4
rhit-sjha3456789 <whshagley@gmail.com>
2025-06-21 10:33:44 -0400

    the changed file

--- project.py
+++ project.py
@@ -1,6 +1,5 @@
 import pygame
 import sys
-
 import random
 import time
 import math
@@ -12,21 +11,13 @@ def distance(point1, point2):
     return math.sqrt((point1_x - point2_x)**2 + (point1_y - point2_y)**2)
 
 def main():
-    # turn on pygame
     pygame.init()
-
-    # create a screen
-    pygame.display.set_caption("NGU's Project" )
-    # TODO: Change the size of the screen as you see fit!
+    pygame.display.set_caption("NGU's Project")
     screen = pygame.display.set_mode((800, 600))
 
-    # creates a Character from the my_character.py file
-
-
-    # let's set the framerate
     clock = pygame.time.Clock()
     while True:
-        clock.tick(60)  # this sets the framerate of your game
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
@@ -37,10 +28,6 @@ def main():
                 print(distance1)
                 if distance1 < screen.get_width()//8:
                     print(distance1)
-
-            # TODO: Add you events code
-
-        # TODO: Fill the screen with whatever background color you like!
         screen.fill((255, 255, 255))
 
         pygame.draw.line(screen, (255, 0, 0), (screen.get_width() // 2, 0), (screen.get_width() // 2, screen.get_height()),
@@ -54,12 +41,7 @@ def main():
         pygame.draw.line(screen, (255, 0, 0), (screen.get_width(), 0), (screen.get_width(), screen.get_height()),
                          8)
         pygame.draw.circle(screen, (0, 0, 0), (screen.get_width()/4, screen.get_height()/2), screen.get_width()/8)
-        # TODO: Add your project code
-
-        # don't forget the update, otherwise nothing will show up!
         pygame.display.update()
 
 
 main()
-
-#test share
