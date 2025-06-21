4a041cd24c982c1ff89e2d8ce898e9d638f59a09
rhit-sjha3456789 <whshagley@gmail.com>
2025-06-21 10:19:12 -0400

    circle YAYYY

--- project.py
+++ project.py
@@ -1,38 +1,38 @@
 import pygame
 import sys
-import my_character
 import random
 import time
-
+import math
+def distance(point1, point2):
+    point1_x = point1[0]
+    point2_x = point2[0]
+    point1_y = point1[1]
+    point2_y = point2[1]
+    return math.sqrt((point1_x - point2_x)**2 + (point1_y - point2_y)**2)
 
 def main():
-    # turn on pygame
     pygame.init()
-
-    # create a screen
     pygame.display.set_caption("NGU's Project")
-    # TODO: Change the size of the screen as you see fit!
     screen = pygame.display.set_mode((800, 600))
-
-    # creates a Character from the my_character.py file
-
-
-    # let's set the framerate
     clock = pygame.time.Clock()
     while True:
-        clock.tick(60)  # this sets the framerate of your game
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mousepos = pygame.mouse.get_pos()
+                print(mousepos)
+                distance1 = distance(mousepos, (screen.get_width()//4, screen.get_height()//2))
+                print(distance1)
+                if distance1 < screen.get_width()//8:
+                    print(distance1)
 
             # TODO: Add you events code
 
-        # TODO: Fill the screen with whatever background color you like!
         screen.fill((255, 255, 255))
 
-        # draws the character every frame
-
-        pygame.draw.line(screen, (255, 0, 0), (screen.get_width() / 2, 0), (screen.get_width() / 2, screen.get_height()),
+        pygame.draw.line(screen, (255, 0, 0), (screen.get_width() // 2, 0), (screen.get_width() // 2, screen.get_height()),
                          10)
         pygame.draw.line(screen, (255, 0, 0), (0, 0), (screen.get_width(), 0),
                          8)
@@ -42,12 +42,10 @@ def main():
                          8)
         pygame.draw.line(screen, (255, 0, 0), (screen.get_width(), 0), (screen.get_width(), screen.get_height()),
                          8)
+        pygame.draw.circle(screen, (0, 0, 0), (screen.get_width()/4, screen.get_height()/2), screen.get_width()/8)
         # TODO: Add your project code
 
-        # don't forget the update, otherwise nothing will show up!
         pygame.display.update()
 
 
 main()
-
-#test share
